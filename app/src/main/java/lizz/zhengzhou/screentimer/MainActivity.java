59d1b5b3b6ad08d4d4ef6c9947f7602d85798733
lizz <374494848@qq.com>
2019-03-28 16:49:30 +0800

    自启动 打开

--- app/src/main/java/lizz/zhengzhou/screentimer/MainActivity.java
+++ app/src/main/java/lizz/zhengzhou/screentimer/MainActivity.java
@@ -8,14 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_open, btn_close;
+    TextView screenStatus;
     DevicePolicyManager policyManager;
     ComponentName adminReceiver;
     boolean IsServiceStarted = false;
+
+    Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +28,27 @@ public class MainActivity extends AppCompatActivity {
 
         btn_open = (Button) findViewById(R.id.btn_open);
         btn_close = (Button) findViewById(R.id.btn_close);
+        screenStatus = (TextView) findViewById(R.id.screenStatus);
 
-        final Intent intent = new Intent(this, BackGroundService.class);
-
+        serviceIntent = new Intent(this, BackGroundService.class);
         adminReceiver = new ComponentName(MainActivity.this, ScreenOffAdminReceiver.class);
 
         btn_open.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!IsServiceStarted) {
-                    boolean admin = policyManager.isAdminActive(adminReceiver);
-                    if (admin) {
-                        startService(intent);
+
+                boolean admin = policyManager.isAdminActive(adminReceiver);
+                if (admin) {
+                    if (!IsServiceStarted) {
+                        startService(serviceIntent);
                         IsServiceStarted = true;
-                    } else {
-                        Toast.makeText(MainActivity.this, "没有设备管理权限",
-                                Toast.LENGTH_LONG).show();
+                        screenStatus.setText("已启动");
                     }
+                } else {
+                    Toast.makeText(MainActivity.this, "没有设备管理权限",
+                            Toast.LENGTH_LONG).show();
                 }
+
             }
         });
 
@@ -49,17 +56,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (IsServiceStarted) {
-                    stopService(intent);
+                    stopService(serviceIntent);
                     IsServiceStarted = false;
+                    screenStatus.setText("已停止");
                 }
             }
         });
 
-        policyManager =  (DevicePolicyManager) MainActivity.this.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        policyManager = (DevicePolicyManager) MainActivity.this.getSystemService(Context.DEVICE_POLICY_SERVICE);
 
         Intent policyIntent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-        policyIntent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN,  adminReceiver);
-        policyIntent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,"开启后就可以使用锁屏功能了...");
+        policyIntent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, adminReceiver);
+        policyIntent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "开启后就可以使用锁屏功能了...");
 
         startActivityForResult(policyIntent, 0);
 
@@ -69,16 +77,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(policyManager.isAdminActive(adminReceiver)){//判断超级管理员是否激活
+        if (policyManager.isAdminActive(adminReceiver)) {//判断超级管理员是否激活
 
-            Toast.makeText(MainActivity.this,"设备已被激活",
+            Toast.makeText(MainActivity.this, "设备已被激活",
                     Toast.LENGTH_LONG).show();
 
-        }else{
+            if (!IsServiceStarted) {
+                startService(serviceIntent);
+                IsServiceStarted = true;
+                screenStatus.setText("已启动");
+            }
+        } else {
 
-            Toast.makeText(MainActivity.this,"设备没有被激活",
+            Toast.makeText(MainActivity.this, "设备没有被激活",
                     Toast.LENGTH_LONG).show();
 
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (IsServiceStarted) {
+            stopService(serviceIntent);
+            IsServiceStarted = false;
+            screenStatus.setText("已停止");
+        }
+        super.onDestroy();
     }
 }
